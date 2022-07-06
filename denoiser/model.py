b0e9f59434aebc9b119510af1675b3d4c0cbf395
penguin1109 <59923223+penguin1109@users.noreply.github.com>
2022-07-06 13:47:39 +0900

    Update model.py

--- denoiser/model.py
+++ denoiser/model.py
@@ -2,3 +2,25 @@
 contains the code for experimental denoiser models
 UNet and the UNet family
 """
+NN_ARCHS = [
+    "unet", "unetpp", "resunet", 
+    "attunet", "asppunet", "nbnet"
+]
+
+
+def get_model(nn_arch , seg = seg, num_channels=1):
+    if nn_arch == "unet8":
+        return UNet(num_channels, num_of_layers=8, seg=seg)
+    elif nn_arch == "unetpp":
+        return UNetPP(num_channels, num_of_layers=12, seg=seg)
+    elif nn_arch == "resunet":
+        return ResUNet(num_channels, num_of_layers=17, seg=seg)
+    elif nn_arch == "attunet":
+        return AttUNet(num_channels, num_of_layers=25, seg=seg)
+    elif nn_arch == "asppunet":
+        return ASPPUNet(num_channels, num_channels, 8, seg=seg)
+    elif nn_arch == "nbnet":
+        return NBNet(num_channels, num_channels, 16, seg=seg)
+    else:
+        print("No supported NN architecture")
+        return None
