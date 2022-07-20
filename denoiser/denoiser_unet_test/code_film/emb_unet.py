11817dc680952a9ad1510e67376feb3f05464529
penguin1109 <59923223+penguin1109@users.noreply.github.com>
2022-07-20 12:53:58 +0900

    Update emb_unet.py

--- denoiser/denoiser_unet_test/code_film/emb_unet.py
+++ denoiser/denoiser_unet_test/code_film/emb_unet.py
@@ -10,8 +10,8 @@ class FeatExtractor(nn.Module):
         ## Using Pretrained ResNext101 as the Feature Extractor
         self.net = model.resnext101_32x8d(pretrained = True)
     def forward(self, x):
-        if x.size(0) == 0:
-            x = torch.cat([x, x, x], dim = 0)
+        if x.size(1) == 1:
+          x = torch.cat([x, x, x], dim = 1)
         return self.net(x)
         
 class FiLM(nn.Module):
@@ -24,8 +24,8 @@ class FiLM(nn.Module):
         b = x.size(0)
         vec = self.lin(x)
         vec = vec.view(b, self.fout//2, 2)
-        gamma = torch.unsqueeze(vec[:, :, 0], dim = -1)
-        beta = torch.unsqueeze(vec[:, :, 1, :], dim = -1)
+        gamma = torch.unsqueeze(torch.unsqueeze(vec[:, :, 0], dim = -1), dim = -1)
+        beta = torch.unsqueeze(torch.unsqueeze(vec[:, :, 1], dim = -1), dim = -1)
         return (img * gamma) + beta
 
 class SELayer(nn.Module):
@@ -39,22 +39,25 @@ class SELayer(nn.Module):
     
     def forward(self, x):
         scale = self.pool(x)
-        scale = torch.squeeze(scale)
+        print(scale.shape)
+        b, c = scale.size(0), scale.size(1)
+        scale = scale.reshape((b, c))
         scale = self.sig(self.lin2(self.relu(self.lin1(scale))))
+        scale = scale.reshape((b, c, 1, 1))
         return scale * x
 
 class EncBlock(nn.Module):
     def __init__(self, ch_in, ch_out, downsample = True):
         super(EncBlock, self).__init__()
         self.ds = downsample
-        self.downsample = nn.Conv2d(ch_in, ch_in, kernel_size = 3, stride = 2, padding = 1)
+        self.downsample = nn.MaxPool2d(kernel_size = 2, stride = 2, padding = 0)
         self.block1 = nn.Sequential(
             nn.Conv2d(ch_in, ch_out, kernel_size = 3, stride = 1, padding = 1),
             nn.InstanceNorm2d(ch_out, affine = True),
             nn.ReLU(inplace = True)
         )
         self.block2 = nn.Sequential(
-            nn.Conv2d(ch_out, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv2d(ch_out,  ch_out, kernel_size = 3, stride = 1, padding = 1),
             nn.InstanceNorm2d(ch_out, affine = True),
             nn.ReLU(inplace = True)
         )
@@ -67,7 +70,7 @@ class DecBlock(nn.Module):
     def __init__(self, ch_in, ch_out, upsample = True):
         super(DecBlock, self).__init__()
         self.us = upsample
-        self.upsample = nn.ConvTranspose2d(ch_in, ch_in, kernel_size = 3, stride = 2, padding = 1)
+        self.upsample = nn.ConvTranspose2d(ch_in, ch_in, kernel_size = 2, stride = 2, dilation = 2, padding = 1, output_padding= 1)
         self.block1 = nn.Sequential(
             nn.Conv2d(ch_in * 2, ch_out, kernel_size = 3, stride = 1, padding = 1),
             nn.InstanceNorm2d(ch_out, affine = True),
@@ -93,21 +96,18 @@ class EmbUNet(nn.Module):
         self.head = nn.Conv2d(ch_in, scale, kernel_size = 3, stride = 1, padding = 1)
         ## Encoder ##
         self.down1 = EncBlock(scale, scale, downsample = False)
-        self.down2 = EncBlock(scale * 2, scale * 4, downsample = True)
-        self.se1 = SELayer(scale * 4, 16)
-        self.down3 = EncBlock(scale * 4, scale * 8, downsample = True)
+        self.down2 = EncBlock(scale, scale * 2, downsample = True)
+        self.se1 = SELayer(scale * 2, 16)
+        self.down3 = EncBlock(scale * 2, scale * 4, downsample = True)
         self.se2 = SELayer(scale * 4, 16)
 
-        self.bridge = EncBlock(scale * 8, scale * 8, downsample = True)
+        self.bridge = EncBlock(scale * 4, scale * 4, downsample = True)
 
-        self.up1 = DecBlock(scale * 8, scale * 4, upsample = True)
-        self.up2 = DecBlock(scale * 4, scale * 2, upsample = True)
-        self.up3 = DecBlock(scale * 2, scale, upsample = True)
+        self.up1 = DecBlock(scale * 4, scale * 2, upsample = True)
+        self.up2 = DecBlock(scale * 2, scale, upsample = True)
+        self.up3 = DecBlock(scale, scale // 2, upsample = True)
 
-        self.tail = nn.Conv2d(scale, ch_in, kernel_size = 1, stride = 1, padding = 0)
-    def forward(self, x):
-
-
+        self.tail = nn.Conv2d(scale // 2, ch_in, kernel_size = 1, stride = 1, padding = 0)
     def forward(self, x):
         head = self.head(x)
         feat = self.feat_ext(x)
@@ -124,7 +124,3 @@ class EmbUNet(nn.Module):
         
         out = self.tail(u3)
         return out
-
-
-
-
